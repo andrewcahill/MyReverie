1f96540d69384762d8d8e28f3176174275a4ecf3
Andrew Cahill <andrew@aviddeveloper.com>
2018-07-02 21:52:40 -0400

    Refactored - removed unnecessary usings

--- IntegrationTest/Services/Goals/GoalsScenarioBase.cs
+++ IntegrationTest/Services/Goals/GoalsScenarioBase.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace IntegrationTest.Services.Goals
 {
