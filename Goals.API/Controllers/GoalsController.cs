42ac1c4007f783b0cc53784a0d32623065c9e9a5
Andrew Cahill <andrew@aviddeveloper.com>
2018-07-13 02:24:33 -0400

    Added versioning to Web API, updated Integration tests for API to pass

--- Goals.API/Controllers/GoalsController.cs
+++ Goals.API/Controllers/GoalsController.cs
@@ -3,37 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Goals.API.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/[controller]")]
     [ApiController]
     public class GoalsController : ControllerBase
     {
-        // GET api/values
+        // GET api/1.0/goals
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/1.0/goals/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/1.0/goals
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/1.0/goals/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/1.0/goals/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
