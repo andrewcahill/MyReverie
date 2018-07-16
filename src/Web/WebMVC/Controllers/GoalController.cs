378276a2b91aad3be09969e691a00a977574df8b
Andrew Cahill <andrew@aviddeveloper.com>
2018-07-16 08:19:53 -0400

    Add MVC UI

---
+++ src/Web/WebMVC/Controllers/GoalController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Services;
+using WebMVC.ViewModels.GoalViewModels;
+
+namespace WebMVC.Controllers
+{
+    public class GoalController : Controller
+    {
+        private readonly IGoalService _goalService;
+
+        public GoalController(IGoalService goalService)
+        {
+            _goalService = goalService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vm = new IndexViewModel()
+            {
+                Goal = await _goalService.GetGoal()
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ src/Web/WebMVC/Services/GoalService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.ViewModels;
+
+namespace WebMVC.Services
+{
+    public class GoalService : IGoalService
+    {
+        public async Task<Goal> GetGoal()
+        {
+            return new Goal { Name = "First Goal" };
+        }
+    }
+}
