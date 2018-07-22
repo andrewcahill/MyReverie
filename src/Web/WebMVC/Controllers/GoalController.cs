978cc3543d00444cd0e8986be65e31cbabc586f8
Andrew Cahill <andrew@aviddeveloper.com>
2018-07-21 22:44:44 -0400

    Added logging

--- src/Web/WebMVC/Controllers/GoalController.cs
+++ src/Web/WebMVC/Controllers/GoalController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WebMVC.Services;
 using WebMVC.ViewModels.GoalViewModels;
 
@@ -11,10 +12,12 @@ namespace WebMVC.Controllers
     public class GoalController : Controller
     {
         private readonly IGoalService _goalService;
+        private readonly ILogger _logger;
 
-        public GoalController(IGoalService goalService)
+        public GoalController(IGoalService goalService, ILogger<GoalController> logger)
         {
             _goalService = goalService;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +26,9 @@ namespace WebMVC.Controllers
             {
                 Goal = await _goalService.GetGoal()
             };
+
+            // Will log something more meaningful then this - this is just test for now
+            _logger.Log(LogLevel.Information, "Called Index method");
 
             return View(vm);
         }
