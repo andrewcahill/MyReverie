27ff10172d67c0348ecb31fe5b9c40885d479d9e
Andrew Cahill <andrew@aviddeveloper.com>
2018-10-10 06:36:11 +0400

    Added Target Date and Description fields to model with associated attributes Added model validation Add link back to home page in edit, delete and details pages

--- src/Web/WebMVC/ViewModels/Goal.cs
+++ src/Web/WebMVC/ViewModels/Goal.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,15 @@ namespace WebMVC.ViewModels
     public class Goal
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName("Target Name")]
+        [DataType(DataType.Date)]
+        public DateTime TargetDate { get; set; }
     }
 }
