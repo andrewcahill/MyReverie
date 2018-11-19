af97dad23dace8dd7b2610008e5e2653cb1a179c
Andrew Cahill <andrew@aviddeveloper.com>
2018-11-20 01:18:57 +0400

    change goals api view to swagger

--- src/Services/Goals/Goals.API/Controllers/HomeController.cs
+++ src/Services/Goals/Goals.API/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Goals.API.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return new RedirectResult("~/swagger");
         }
     }
 }
